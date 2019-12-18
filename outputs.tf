f67df3a7897657a4c89a7023077a92cd6bf7959f
Boris Schrijver <bschrijver@schubergphilis.com>
2019-12-18 11:00:22 +0100

    Add okta_idp_arn output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "assume_policy" {
   value       = data.aws_iam_policy_document.okta_assume_policy.json
   description = "The IAM policy you can attach to roles so they can be assumed by Okta"
 }
+
+output "okta_idp_arn" {
+  value       = aws_iam_saml_provider.default.arn
+  description = "The ARN of the Okta Identity Provider in AWS"
+}
